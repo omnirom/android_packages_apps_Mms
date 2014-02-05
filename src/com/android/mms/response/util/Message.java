a293ef4e079c85742978fe745c649489c0f37296
ByteHamster <info@hanspeter-lehmann.de>
2014-02-05 15:01:50 +0100

    MMS: Add feature for sending auto response      Add feature to vibrate message in morse code      (Fix spaces)
    Change-Id: I55862204ef71f69bc88c79fe2259f7cb8365798a

---
+++ src/com/android/mms/response/util/Message.java
@@ -0,0 +1,54 @@
+/*
+ *  Copyright (C) 2013 The OmniROM Project
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+package com.android.mms.response.util;
+
+public class Message {
+    private String title;
+    private String content;
+
+    public Message() {
+        title = "";
+        content = "";
+    }
+
+    public Message(String title, String content) {
+        this.title = title;
+        this.content = content;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public void setInfo(String title, String content) {
+        this.title = title;
+        this.content = content;
+    }
+}
---
+++ src/com/android/mms/response/util/PhoneContact.java
@@ -0,0 +1,66 @@
+/*
+ *  Copyright (C) 2013 The OmniROM Project
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+package com.android.mms.response.util;
+
+public class PhoneContact {
+    private String name;
+    private String number;
+    private String id;
+
+    public PhoneContact() {
+        name = "";
+        number = "";
+        id = "";
+    }
+
+    public PhoneContact(String name, String number, String id) {
+        this.name = name;
+        this.number = number;
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getNumber() {
+        return number;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setNumber(String number) {
+        this.number = number;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public void setInfo(String name, String number, String id) {
+        this.name = name;
+        this.number = number;
+        this.id = id;
+    }
+}
