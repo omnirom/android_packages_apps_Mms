f0e5c76c3d978df11333e91e12302da9c9aa3bba
ByteHamster <info@hanspeter-lehmann.de>
2014-02-05 15:01:50 +0100

    MMS: Enter key: force newline
    Provide an option to force newline (instead of send).
    For some keyboards (eg. SwiftKey) the shift+enter solution doesn't work.
    
    Change-Id: I16c16df9e94fd334785e830091be44a5237db48a

--- src/com/android/mms/ui/ComposeMessageActivity.java
+++ src/com/android/mms/ui/ComposeMessageActivity.java
@@ -388,6 +388,9 @@ public class ComposeMessageActivity extends Activity
     public final static String THREAD_ID = "thread_id";
     private final static String RECIPIENTS = "recipients";
 
+    // Force the enter key to add a newline (default: false)
+    private boolean mEnterForNewline;
+
     @SuppressWarnings("unused")
     public static void log(String logMsg) {
         Thread current = Thread.currentThread();
@@ -1945,6 +1948,7 @@ public class ComposeMessageActivity extends Activity
                 MessagingPreferenceActivity.UNICODE_STRIPPING_LEAVE_INTACT);
         mInputMethod = Integer.parseInt(prefs.getString(MessagingPreferenceActivity.INPUT_TYPE,
                 Integer.toString(InputType.TYPE_TEXT_VARIATION_SHORT_MESSAGE)));
+        mEnterForNewline = prefs.getBoolean("pref_key_mms_enter_new_line", false);
 
         mLibrary = TemplateGesturesLibrary.getStore(this);
 
@@ -3530,7 +3534,7 @@ public class ComposeMessageActivity extends Activity
         if (event != null) {
             // if shift key is down, then we want to insert the '\n' char in the TextView;
             // otherwise, the default action is to send the message.
-            if (!event.isShiftPressed() && event.getAction() == KeyEvent.ACTION_DOWN) {
+            if (!event.isShiftPressed() && event.getAction() == KeyEvent.ACTION_DOWN && !mEnterForNewline) {
                 if (isPreparedForSending()) {
                     confirmSendMessageIfNeeded();
                 }
