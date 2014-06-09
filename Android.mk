40cb19c22d2ee3826f6b120a4bff599ed92ba6e1
ByteHamster <info@bytehamster.com>
2014-06-09 14:28:25 +0200

    [WIP] Mms: Use the nice colorful LetterTileDefaultImages introduced in 4.4.3
    Android.mk is now 99% the same like Dialer/Android.mk
    but it does not build because of this error:
    
    ab/ContactsCommon/xy/ab.java: error: package R does not exist
    
    Change-Id: I280a5e3abb8dffb9340d360a99da966ea48746f1

--- Android.mk
+++ Android.mk
@@ -2,34 +2,43 @@
 
 LOCAL_PATH:= $(call my-dir)
 include $(CLEAR_VARS)
-# Include res dir from chips
-chips_dir := ../../../frameworks/ex/chips/res
-res_dirs := $(chips_dir) res
-
-$(shell rm -f $(LOCAL_PATH)/chips)
 
 LOCAL_MODULE_TAGS := optional
 
-LOCAL_SRC_FILES := $(call all-java-files-under, src)
+contacts_common_dir := ../ContactsCommon
+chips_dir := ../../../frameworks/ex/chips
 
-LOCAL_PACKAGE_NAME := Mms
+src_dirs := src $(contacts_common_dir)/src
+res_dirs := res $(contacts_common_dir)/res $(chips_dir)/res
 
-# Builds against the public SDK
-#LOCAL_SDK_VERSION := current
+$(shell rm -f $(LOCAL_PATH)/chips)
+
+LOCAL_SRC_FILES := $(call all-java-files-under, $(src_dirs))
+LOCAL_RESOURCE_DIR := $(addprefix $(LOCAL_PATH)/, $(res_dirs))
+
+LOCAL_AAPT_FLAGS := \
+    --auto-add-overlay \
+    --extra-packages com.android.contacts.common \
+    --extra-packages com.android.ex.chips
 
 LOCAL_JAVA_LIBRARIES += telephony-common mms-common
-LOCAL_STATIC_JAVA_LIBRARIES += android-common jsr305
-LOCAL_STATIC_JAVA_LIBRARIES += android-common-chips
-
-LOCAL_RESOURCE_DIR := $(addprefix $(LOCAL_PATH)/, $(res_dirs))
-LOCAL_AAPT_FLAGS := --auto-add-overlay
-LOCAL_AAPT_FLAGS += --extra-packages com.android.ex.chips
+LOCAL_STATIC_JAVA_LIBRARIES := \
+    com.android.phone.shared \
+    com.android.services.telephony.common \
+    com.android.vcard \
+    android-common \
+    guava \
+    android-support-v13 \
+    android-support-v4 \
+    android-common jsr305 \
+    android-common-chips \
 
 LOCAL_REQUIRED_MODULES := SoundRecorder
 
-LOCAL_PROGUARD_FLAG_FILES := proguard.flags
-
+LOCAL_PACKAGE_NAME := Mms
 LOCAL_PRIVILEGED_MODULE := true
+
+LOCAL_PROGUARD_FLAG_FILES := proguard.flags
 
 include $(BUILD_PACKAGE)
 
